9e875ddfecfc73d06be04162564bd81bea50a16b
MrILL <ilua2403@ukr.net>
2023-06-18 22:05:06 +0300

    fix(backend): use default console logging for errors

--- backend/src/logger/logger.ts
+++ backend/src/logger/logger.ts
@@ -1,6 +1,7 @@
 import { WinstonModule } from 'nest-winston'
 import winston from 'winston'
 import colors from 'colors/safe'
+import { inspect } from 'util'
 
 const format = winston.format.combine(
   winston.format.timestamp({
@@ -36,25 +37,46 @@ const simpleFormat = winston.format.combine(
   }),
 )
 
-export const logger = WinstonModule.createLogger({
-  transports: [
+const winstonTransports = [
+  new winston.transports.Console({
+    format,
+    level: process.env.NODE_ENV === 'development' ? 'debug' : 'info',
+  }),
+  new winston.transports.File({
+    filename: `info-${new Date().toISOString()}.log`,
+    dirname: 'logs',
+    handleExceptions: true,
+    level: 'info',
+    format: simpleFormat,
+  }),
+  new winston.transports.File({
+    filename: `error-${new Date().toISOString()}.log`,
+    dirname: 'logs',
+    handleExceptions: true,
+    level: 'error',
+    format: simpleFormat,
+  }),
+]
+
+if (process.env.NODE_ENV !== 'production') {
+  winstonTransports.push(
     new winston.transports.Console({
-      format,
-      level: process.env.NODE_ENV === 'development' ? 'debug' : 'info',
-    }),
-    new winston.transports.File({
-      filename: `info-${new Date().toISOString()}.log`,
-      dirname: 'logs',
-      handleExceptions: true,
-      level: 'info',
-      format: simpleFormat,
-    }),
-    new winston.transports.File({
-      filename: `error-${new Date().toISOString()}.log`,
-      dirname: 'logs',
-      handleExceptions: true,
       level: 'error',
-      format: simpleFormat,
+      handleExceptions: true,
+      format: winston.format.printf(info => {
+        const { level, message, timestamp, stack } = info
+
+        if (info instanceof Error) {
+          const formattedStack = stack || inspect(info, { depth: null })
+          return `${timestamp} ${level}: ${formattedStack}`
+        }
+
+        return `${timestamp} ${level}: ${message}`
+      }),
     }),
-  ],
+  )
+}
+
+export const logger = WinstonModule.createLogger({
+  transports: winstonTransports,
 })
